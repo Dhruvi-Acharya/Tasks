7086220f8baf9b0ada0a527a645a8d370155606a
Dhruvi-Acharya <Dhruvi-Acharya@einfochips.com>
2022-05-30 12:57:12 +0530

    Modified files

--- include/Books.h
+++ include/Books.h
@@ -1,21 +1,31 @@
 //! header guard
 #ifndef BOOKOPERATIONS_H
 #define BOOKOPERATIONS_H
+#include <list>
+#include <iterator>
+
 //! Books class
 class Books
 {
-  public:
-    int book_stock[10], book_isbn_no[10], book_initial_stock[10]; /**< variable that store book stock, book ISBN number and initial stock */
-    float book_price[10];                                         /**< variable that store book price */
-    string book_author[10], book_name[10];                        /**< array to store book author and book name */
-    int i=0; 
+protected:
+  struct Book
+  {
+    int book_stock, book_isbn_no, book_initial_stock, book_issued = 0; /**< variable that store book stock, book ISBN number and initial stock */
+    float book_price;                                                  /**< variable that store book price */
+    string book_author, book_name;                                     /**< array to store book author and book name */
+  } bookObj;
 
-    void welcomemsg();                                                                              //!< declaration of welcome message function
-    void addBook(string bookName, string bookAuthor, int bookStock, int bookISBN, float bookPrice); //!< declaration of add book into the library
-    void display();                                                                                 //!< declaration of display all the availble books into the library
-    void display(string bookAuthor);                                                                //!< declaration of display all the availble books of given book name
-    void issueBook(string bookName);                                                                //!< declaration of function that used for issue book from the library
-    void returnBook(string bookName);                                                               //!< declaration of function that used to return book to the library
-    void deleteBook(string bookName);
+  list<Book> bookList; //!< STL list to datatype Book
+  list<Book>::iterator it;
+  int i = 0;
+
+public:
+  void welcomemsg();                                                                              //!< declaration of welcome message function
+  void addBook(string bookName, string bookAuthor, int bookStock, int bookISBN, float bookPrice); //!< declaration of add book into the library
+  void display();                                                                                 //!< declaration of display all the availble books into the library
+  void display(string bookAuthor);                                                                //!< declaration of display all the availble books of given book name
+  void issueBook(string bookName);                                                                //!< declaration of function that used for issue book from the library
+  void returnBook(string bookName);                                                               //!< declaration of function that used to return book to the library
+  void deleteBook(string bookName);
 };
 #endif
--- include/CountBooks.h
+++ include/CountBooks.h
@@ -1,13 +1,14 @@
 /*!including operations header file*/
 #include "Books.h"
-//!header guard
+//! header guard
 #ifndef COUNTBOOKS_H
 #define COUNTBOOKS_H
 /*!class CountBooks inheriting the class Books*/
-class CountBooks: public Books {
-  public:
-    void welcomemsg();                //!< declaration of print welcome message function
-    void countBook();                 //!< declaration of count total books into the library
-    void countBook(string bookName);  //!< declaration of count book by given by book name
+class CountBooks : public Books
+{
+public:
+  void welcomemsg();               //!< declaration of print welcome message function
+  void countBook();                //!< declaration of count total books into the library
+  void countBook(string bookName); //!< declaration of count book by given by book name
 };
 #endif
