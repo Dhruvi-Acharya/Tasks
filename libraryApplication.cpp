3c62c57415011e620fc17f8e84f6f7ebe1a73f07
Dhruvi-Acharya <Dhruvi-Acharya@einfochips.com>
2022-05-30 12:57:47 +0530

    Main File

--- libraryApplication.cpp
+++ libraryApplication.cpp
@@ -1,7 +1,7 @@
 /*****************************************************************************************************
-* Library System
-* functonalities: add new book into the library, search book, issue book, return book and delete book
-******************************************************************************************************/
+ * Library System
+ * functonalities: add new book into the library, search book, issue book, return book and delete book
+ ******************************************************************************************************/
 
 /*! including input output stream header file */
 #include <iostream>
@@ -12,91 +12,104 @@ using namespace std;
 
 int main()
 {
+	enum bookOperation
+	{
+		addBook = 1,
+		displayAllBooks = 2,
+		displayBookByAuthor = 3,
+		countTotalBooks = 4,
+		countParticularBook = 5,
+		issueBook = 6,
+		returnBook = 7,
+		deleteBook = 8,
+		quit = 9
+	};
 	Books *ptrBook;
 	CountBooks objBook; /**< created object of CountBooks */
 	ptrBook = &objBook;
-	
+
 	string bookName, bookAuthor;
-	int bookStock,bookISBNno;
+	int bookStock, bookISBNno;
 	float bookPrice;
-	
+
 	int choice; /**< for user choice to perform operation*/
-	
+
 	do
 	{
 		ptrBook->welcomemsg(); /*! printing welcome message */
 
-		cout << endl<<"1. Enter book into the Library\n2. Disply all the books\n3. Display all books of given author\n4. Display total number of books in the library\n5. Display total number of available by book name\n6. Issue Book\n7. Return Book\n8. Delete Book\n9. Exit\n";
+		cout << endl
+			 << "1. Enter book into the Library\n2. Disply all the books\n3. Display all books of given author\n4. Display total number of books in the library\n5. Display total number of available by book name\n6. Issue Book\n7. Return Book\n8. Delete Book\n9. Exit\n";
 
-		cout <<"\nEnter your choice: ";
+		cout << "\nEnter your choice: ";
 		cin >> choice; /*! taking choice from user */
-		cout<<endl;
+		cout << endl;
 
 		switch (choice)
 		{
-			case 1:
-				cout<<"Enter book name: ";
-            	cin>>bookName;
-            	cout<<"Enter book author: ";
-            	cin>>bookAuthor;
-            	cout<<"Enter book stock: ";
-            	cin>>bookStock;
-            	cout<<"Enter book ISBN no.: ";
-            	cin>>bookISBNno;
-            	cout<<"Enter book price: ";
-            	cin>>bookPrice;
-				
-				objBook.addBook(bookName,bookAuthor,bookStock,bookISBNno,bookPrice); /*! add book into the library */
-            	break;
+		case addBook:
+			cout << "Enter book name: ";
+			cin >> bookName;
+			cout << "Enter book author: ";
+			cin >> bookAuthor;
+			cout << "Enter book stock: ";
+			cin >> bookStock;
+			cout << "Enter book ISBN no.: ";
+			cin >> bookISBNno;
+			cout << "Enter book price: ";
+			cin >> bookPrice;
 
-			case 2:
-				objBook.display(); /*! display all books information from the library */
-				break;
+			objBook.addBook(bookName, bookAuthor, bookStock, bookISBNno, bookPrice); /*! add book into the library */
+			break;
 
-			case 3:
-				cout << "Enter Author name: ";
-				cin >> bookAuthor;
+		case displayAllBooks:
+			objBook.display(); /*! display all books information from the library */
+			break;
 
-				objBook.display(bookAuthor); /*! display infromation book by book author */
-				break;
+		case displayBookByAuthor:
+			cout << "Enter Author name: ";
+			cin >> bookAuthor;
 
-			case 4:
-				objBook.countBook(); /*! adisplay total number of books availble in the library */
-				break;
+			objBook.display(bookAuthor); /*! display infromation book by book author */
+			break;
 
-			case 5:
-				cout << "Enter book name which you want to show count: ";
-				cin >> bookName;
-				objBook.countBook(bookName); /*! display particluar book name count */
-				break;
+		case countTotalBooks:
+			objBook.countBook(); /*! adisplay total number of books availble in the library */
+			break;
 
-			case 6:
-				cout << "Enter book name which you want to issue: ";
-				cin >> bookName;
+		case countParticularBook:
+			cout << "Enter book name which you want to show count: ";
+			cin >> bookName;
+			objBook.countBook(bookName); /*! display particluar book name count */
+			break;
 
-				objBook.issueBook(bookName); /*! issue book from the library */
-				break;
+		case issueBook:
+			cout << "Enter book name which you want to issue: ";
+			cin >> bookName;
 
-			case 7:
-				cout << "Enter book name which you want to return: ";
-				cin >> bookName;
+			objBook.issueBook(bookName); /*! issue book from the library */
+			break;
 
-				objBook.returnBook(bookName); /*! return book to the library */
-				break;
+		case returnBook:
+			cout << "Enter book name which you want to return: ";
+			cin >> bookName;
 
-			case 8:
-				cout << "Enter book name which you want to delete: ";
-				cin >> bookName;
+			objBook.returnBook(bookName); /*! return book to the library */
+			break;
 
-				objBook.deleteBook(bookName); /*! delete book from the library */
-				break;
+		case deleteBook:
+			cout << "Enter book name which you want to delete: ";
+			cin >> bookName;
 
-			case 9:
-				cout<<"\nThank you for visiting us"<<endl;
-				exit(0); /*! quitting user from the application*/
+			objBook.deleteBook(bookName); /*! delete book from the library */
+			break;
 
-			default:
-				cout << "\nPlease Enter valid choice" << endl;
+		case quit:
+			cout << "Thank you for visiting us" << endl;
+			exit(0); /*! quitting user from the application*/
+
+		default:
+			cout << "Please Enter valid choice" << endl;
 		}
 	} while (choice != 9);
 	return 0;
